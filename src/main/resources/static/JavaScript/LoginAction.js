730c00af85ef36bafa6a0ce93882b0a342d23368
qinghe <2980267045@qq.com>
2021-10-24 11:54:23 +0800

    Updata 10.24

---
+++ src/main/resources/static/JavaScript/LoginAction.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-24 09:20:58
+ * @LastEditTime: 2021-10-24 11:27:34
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \workplace_bcl\our-web\src\main\resources\static\JavaScript\LoginAction.js
+ */
