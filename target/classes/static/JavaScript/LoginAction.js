1ef45d923d3cf47c4b1c211bce3491f7753f61d5
xrZhang35 <zxr35l@163.com>
2021-12-12 18:47:10 +0800

    Delete ignored files.

--- target/classes/static/JavaScript/LoginAction.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2021-10-24 09:20:58
- * @LastEditTime: 2021-10-24 11:27:34
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \workplace_bcl\our-web\src\main\resources\static\JavaScript\LoginAction.js
- */
